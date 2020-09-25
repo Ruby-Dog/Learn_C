b5ab9631e8416754461e55bb05f36f53ff8d27c1
RubyDog <65643745+Ruby-Dog@users.noreply.github.com>
2020-09-25 15:07:31 +0800

    Create 20200925.c

---
+++ 20200925.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int sumOfSquare(int ,int );
+void main(void){
+	int myAnswer = sumOfSquare(1,10);
+	printf("The sum of 1^+2^+3^....10^ is = %d\n",myAnswer);
+	return ;
+	
+}
+int sumOfSquare(int firstNum,int lastNum){
+	int sum=0 ;
+	
+	int i=0;
+	for(i=firstNum;i<=lastNum;i++){
+		int i_square = i*i;
+		sum+=i_square;	
+	}
+	return sum;
+}
